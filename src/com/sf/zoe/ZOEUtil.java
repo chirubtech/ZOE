f5db2058d07ca1a61e6cb697362390a1057cb430
chirubtech <chiru.btech@gmail.com>
2017-10-06 19:35:35 +0530

    Update ZOEUtil.java

--- src/com/sf/zoe/ZOEUtil.java
+++ src/com/sf/zoe/ZOEUtil.java
@@ -1,6 +1,9 @@
 package com.sf.zoe;
 
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -11,6 +14,8 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -24,51 +29,92 @@ import org.jfree.ui.RefineryUtilities;
 import org.json.JSONArray;
 
 import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamPanel;
+import com.xuggle.mediatool.IMediaWriter;
+import com.xuggle.mediatool.ToolFactory;
+import com.xuggle.xuggler.ICodec;
+import com.xuggle.xuggler.IPixelFormat;
+import com.xuggle.xuggler.IVideoPicture;
+import com.xuggle.xuggler.video.ConverterFactory;
+import com.xuggle.xuggler.video.IConverter;
 
 class CaptureThread implements Runnable {
-	final Webcam webcam = Webcam.getDefault();
-	private BlockingQueue<BufferedImage> queue;
+	private BlockingQueue<ImageVo> queue;
+	private volatile boolean stopFlag = false;
+	static final String OUT_FILE_NAME = "out.mpg";
+	private Webcam webcam;
 
-	private int interval;
-
-	public CaptureThread(BlockingQueue<BufferedImage> queue, int interval) {
+	public CaptureThread(BlockingQueue<ImageVo> queue, int interval, Webcam webcam) {
 		this.queue = queue;
-		this.interval = interval;
+		this.webcam = webcam;
 
+	}
+
+	public void setStopFlag(boolean stopFlag) {
+		this.stopFlag = stopFlag;
 	}
 
 	@Override
 	public void run() {
 		try {
-			webcam.setViewSize(new Dimension(640, 480));
-			System.out.println(webcam.getDevice().getName());
-			webcam.open();
-			while (true) {
-				// ImageIO.write(webcam.getImage(), "JPG", new File(name));
-				this.queue.put(webcam.getImage());
-				System.out.println("Image added to queue at " + System.currentTimeMillis());
-				Thread.sleep(interval);
-			}
 
+			File file = new File(OUT_FILE_NAME);
+
+			IMediaWriter writer = ToolFactory.makeWriter(file.getName());
+			Dimension size = new Dimension(640, 480);// WebcamResolution.QVGA.getSize();
+
+			writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_MPEG1VIDEO, size.width, size.height);
+			// writer.addVideoStream(0, 0, ICodec.ID.CODEC_ID_MPEG4, size.width,
+			// size.height);
+
+			long start = System.currentTimeMillis();
+			long current = start;
+			for (int i = 0; !stopFlag; i++) {
+				// System.out.println("Capture frame " + i);
+				BufferedImage image = ConverterFactory.convertToType(webcam.getImage(), BufferedImage.TYPE_3BYTE_BGR);
+				if ((System.currentTimeMillis() - current) > 1000) {
+					this.queue.put(new ImageVo(image, System.currentTimeMillis() - start));
+					current = System.currentTimeMillis();
+				}
+				IConverter converter = ConverterFactory.createConverter(image, IPixelFormat.Type.YUV420P);
+				IVideoPicture frame = converter.toPicture(image, (System.currentTimeMillis() - start) * 1000);
+				frame.setKeyFrame(i == 0);
+				frame.setQuality(0);
+
+				writer.encodeVideo(0, frame);
+
+				// 10 FPS
+				Thread.sleep(50);
+			}
+			System.out.println(System.currentTimeMillis() - start);
+			writer.close();
+			System.out.println("Video recorded in file: " + file.getAbsolutePath());
 		} catch (InterruptedException e) {
 			// TODO need to add proper logging
 			e.printStackTrace();
 		}
 	}
-
 }
 
 class ProcessThread implements Runnable {
-	private BlockingQueue<BufferedImage> queue;
-	private int interval;
+	private BlockingQueue<ImageVo> queue;
 
 	static final String ENDPOINT_URL = "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize";
 	static final String[] API_KEYS = { "13ae61eaa7064561994fbb26e45798b6", "470b9dbd4fb74dff9ed9545dd40218e9",
-			"a204c19086d84ae2b1c65c39834ae78e", "542ce8daf19e4c03bb5214311793a7f3" };
+			"a204c19086d84ae2b1c65c39834ae78e", "542ce8daf19e4c03bb5214311793a7f3", "46cf72f2590c4dc59ad7823b048c754b",
+			"eb51057442a448feb9660ab5da69f266",
+			// "abf7e6d8a2624c9eacc33eaad5669eae", "965e8669a92844f0af8c7e9e10aa850a",
+			"546f5f12c021414c9328fc531fb698fc", "2ea94f49c940452d8428f8a2eab4f2a9" };
 	private URIBuilder uriBuilder;
 	private CloseableHttpClient httpclient;
+	private volatile boolean stopFlag = false;
+	private int interval;
 
-	public ProcessThread(BlockingQueue<BufferedImage> queue, int interval) {
+	public void setStopFlag(boolean stopFlag) {
+		this.stopFlag = stopFlag;
+	}
+
+	public ProcessThread(BlockingQueue<ImageVo> queue, int interval) {
 		this.queue = queue;
 		this.interval = interval;
 		try {
@@ -82,35 +128,39 @@ class ProcessThread implements Runnable {
 	@Override
 	public void run() {
 		try {
-			BufferedImage bImage;
+			ImageVo bImage;
 			String fileName = "";
 			// consuming BufferedImages until exit BufferedImage is received
 			int requestCount = 1;
 
 			// Creating chart
-			Linechart chart = new Linechart("Multiple Dataset Demo 1");
+			Linechart chart = new Linechart("");
 			chart.pack();
 			RefineryUtilities.centerFrameOnScreen(chart);
 			chart.setVisible(true);
-			long currTime = System.currentTimeMillis();
 			while ((bImage = queue.take()) != null) {
-				fileName = String.valueOf(System.currentTimeMillis());
+				fileName = String.valueOf(bImage.getTime());
 				try {
-					ImageIO.write(bImage, "JPG", new File(fileName));
-					System.out.println("********" + requestCount % API_KEYS.length);
-					int timeIndex = (int) (System.currentTimeMillis() - currTime);
-					JSONArray json = processImage(bImage, API_KEYS[requestCount % API_KEYS.length]);
+					// ImageIO.write(bImage.getBi(), "JPG", new File(fileName));
+					int timeIndex = (int) (bImage.getTime());
+					JSONArray json = processImage(bImage.getBi(), API_KEYS[requestCount % API_KEYS.length]);
 					System.out.println(json);
-					if (json.length() > 0) {
+					if (json != null && json.length() > 0) {
 						chart.addDataPoint(JsonUtil.getScores(json), timeIndex / 1000);
 					}
 					requestCount++;
+					System.out.println("queue.size(): " + queue.size());
+					if (queue.size() == 0 && stopFlag) {
+						// break;
+					}
+
 				} catch (IOException | URISyntaxException e) {
 					System.out.println("Error in saving file in producer");
 					e.printStackTrace();
 				}
 				Thread.sleep(interval);
 			}
+			chart.dispose();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -139,34 +189,81 @@ class ProcessThread implements Runnable {
 		ByteArrayEntity reqEntity = new ByteArrayEntity(img);
 
 		httppost.setEntity(reqEntity);
-
+		long start = System.currentTimeMillis();
 		HttpResponse response = httpclient.execute(httppost);
 		HttpEntity entity = response.getEntity();
 		if (entity != null) {
-
 			String str = EntityUtils.toString(entity);
 			try {
+				System.out.println(System.currentTimeMillis() - start);
+				// System.exit(0);
 				return new JSONArray(str);
 			} catch (Exception e) {
-				System.out.println("Received Wrong error message: " + str + "\n" + e);
+				System.out.println("Received Wrong error message: " + str + "\n" + e + " Key: " + key);
 			}
 		}
 		return null;
 	}
 }
 
+/**
+ * 
+ * @author cmuttoju
+ *
+ */
 public class ZOEUtil {
 	private static final int INTERVAL = 1 * 1000; // in seconds
 
 	public static void main(String[] args) {
 		// Creating BlockingQueue of size 10
-		BlockingQueue<BufferedImage> queue = new ArrayBlockingQueue<>(10);
-		CaptureThread producer = new CaptureThread(queue, INTERVAL);
+		JFrame window = new JFrame("Capture Panel");
+		BlockingQueue<ImageVo> queue = new ArrayBlockingQueue<>(10);
+
+		Webcam webcam = Webcam.getDefault();
+		webcam.setViewSize(new Dimension(640, 480));
+		webcam.open(true);
+
+		WebcamPanel panel = new WebcamPanel(webcam);
+		panel.setFPSDisplayed(true);
+		panel.setDisplayDebugInfo(true);
+		panel.setImageSizeDisplayed(true);
+		panel.setMirrored(true);
+		window.add(panel);
+		CaptureThread producer = new CaptureThread(queue, INTERVAL, webcam);
 		ProcessThread consumer = new ProcessThread(queue, INTERVAL);
-		new Thread(producer).start();
-		System.out.println("CaptureThread started!!");
-		new Thread(consumer).start();
-		System.out.println("ProcessThreadß started!!");
+
+		window.setSize(640, 600);
+		window.setLayout(new FlowLayout());
+		JButton start = new JButton("START");
+
+		start.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				start.setEnabled(false);
+				new Thread(producer).start();
+				System.out.println("CaptureThread started!!");
+				new Thread(consumer).start();
+				System.out.println("ProcessThread started!!");
+
+			}
+		});
+		JButton stop = new JButton("STOP");
+		stop.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				producer.setStopFlag(true);
+				consumer.setStopFlag(true);
+				// window.dispose();
+
+			}
+		});
+		window.add(start);
+		window.add(stop);
+		window.setResizable(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.pack();
+		window.setVisible(true);
+
 	}
 
 }
